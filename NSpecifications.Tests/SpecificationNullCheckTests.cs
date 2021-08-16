ec706f1c41df29f0ce76903d4c02ddeb0f8df004
Alan Parr <alan.parr@cloudcommercegroup.com>
2021-08-16 17:02:47 +0100

    Updated to netstandard 2.0

---
+++ NSpecifications.Tests/SpecificationNullCheckTests.cs
@@ -0,0 +1,33 @@
+﻿using NSpecifications.Tests.Entities;
+using NUnit.Framework;
+using System;
+
+namespace NSpecifications.Tests
+{
+    [TestFixture]
+    public class SpecificationNullCheckTests
+    {
+        [TestCase(true, false, true)]
+        [TestCase(false, true, true)]
+        [TestCase(false, false, false)]
+        [TestCase(true, true, true)]
+        public void OrSpecificationNullCheck(bool spec1Null, bool spec2Null, bool expectException)
+        {
+            ISpecification<Drink> spec1 = spec1Null ? null : new Spec<Drink>(d => d.Name.ToLower().Contains("juice"));
+            ISpecification<Drink> spec2 = spec2Null ? null : new Spec<Drink>(d => d.Name.ToLower().Contains("juice"));
+
+            if (expectException)
+            {
+                Assert.Throws<ArgumentNullException>(() =>
+                {
+                    spec1.Or(spec2);
+                });
+            }
+            else
+            {
+                var result = spec1.Or(spec2);
+                Assert.IsNotNull(result);
+            }
+        }
+    }
+}
--- Nspecifications/OrSpecification.cs
+++ Nspecifications/OrSpecification.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NSpecifications
 {
@@ -15,24 +11,12 @@ namespace NSpecifications
 
         internal OrSpecification(ISpecification<TEntity> spec1, ISpecification<TEntity> spec2)
         {
-            if (spec1 == null)
-                throw new ArgumentNullException("spec1");
-
-            if (spec2 == null)
-                throw new ArgumentNullException("spec2");
-
-            Spec1 = spec1;
-            Spec2 = spec2;
+            Spec1 = spec1 ?? throw new ArgumentNullException(nameof(spec1));
+            Spec2 = spec2 ?? throw new ArgumentNullException(nameof(spec2));
         }
 
-        public Expression<Func<TEntity, bool>> Expression
-        {
-            get { return Spec1.Expression.Or(Spec2.Expression); }
-        }
+        public Expression<Func<TEntity, bool>> Expression => Spec1.Expression.Or(Spec2.Expression);
 
-        public bool IsSatisfiedBy(TEntity candidate)
-        {
-            return Spec1.IsSatisfiedBy(candidate) || Spec2.IsSatisfiedBy(candidate);
-        }
+        public bool IsSatisfiedBy(TEntity candidate) => Spec1.IsSatisfiedBy(candidate) || Spec2.IsSatisfiedBy(candidate);
     }
 }
